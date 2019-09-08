547beb8ccd3709c839482cc14d63b53eaec9ec23
ri52dksla <34932735+ri52dksla@users.noreply.github.com>
2019-09-08 10:22:06 +0900

    change the order of word in resource name

--- modules/eks/eks_cluster_iam_policies/main.tf
+++ modules/eks/eks_cluster_iam_policies/main.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy_eks_cluster_role_attachment" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_AmazonEKSClusterPolicy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = var.iam_role_name
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy_eks_cluster_role_attachment" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_AmazonEKSServicePolicy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = var.iam_role_name
 }
